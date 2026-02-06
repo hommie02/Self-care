361a52d1e02bf69af5462c9316e3fc883b42a454
hommie02 <hommie02@github.com>
2026-02-06 14:39:01 +0300

    Remove Journal, Activities, Settings screens and navigation - keeping only Home screen

--- App.tsx
+++ App.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import Home from './screens/Home';
-import Activities from './screens/Activities';
-import Journal from './screens/Journal';
-import Settings from './screens/Settings';
-import CurvedTabBar from './components/CurvedTabBar';
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <StatusBar style="light" backgroundColor="#FFB6C1" />
-        <Tab.Navigator
-          tabBar={(props) => <CurvedTabBar {...props} />}
-          screenOptions={{
-            headerShown: false,
-            tabBarStyle: { display: 'none' }, // Hide default tab bar
-          }}
-        >
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Activities" component={Activities} />
-          <Tab.Screen name="Journal" component={Journal} />
-          <Tab.Screen name="Settings" component={Settings} />
-        </Tab.Navigator>
+        <Home />
       </NavigationContainer>
     </SafeAreaProvider>
   );
